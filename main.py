244329e0471d50181e29226c4e003bd9280ca78d
MatthewM5293 <mmunozramirez@student.neumont.edu>
2024-08-12 09:13:42 -0600

    Moves Input Files to Directory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import FileManager
 
 if __name__ == '__main__':
-    FileManager.App(txt_file="Assemblyv3.txt", output_file="Kernel7.img")
+    FileManager.App(txt_file="Input Files/Assembly_v3.txt", output_file="Kernel7.img")
 
